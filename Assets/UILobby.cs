76c046a23f277574af68df1d3cefe2d3e221ae3b
ShAdowDev16 <finn@boomgaardt.be>
2025-04-05 22:20:53 +0200

    Worked on main menu (not finished)

---
+++ Assets/UILobby.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class UILobby : MonoBehaviour
+{
+    public TextMeshProUGUI Name;
+    public Button JoinButton;
+    public string ID;
+}
