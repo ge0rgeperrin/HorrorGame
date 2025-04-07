525c6b37ada27997084f1465c4af9d5f722b77cd
THeTechWiz5305 <132825368+TheTechWiz5305@users.noreply.github.com>
2025-04-07 19:26:42 -0400

    SubterraneaUtils.cs

---
+++ Assets/Scripts/SubterraneaUtils.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using Steamworks;
+using UnityEngine.XR;
+
+public static class SubterraneaUtils
+{
+    private static Platform GetPlatform()
+    {
+        var platform = Application.platform;
+
+        if (platform == RuntimePlatform.WindowsPlayer || platform == RuntimePlatform.WindowsEditor)
+            return XRSettings.enabled ? Platform.WindowsVR : Platform.WindowsPC;
+
+        if (platform == RuntimePlatform.OSXPlayer || platform == RuntimePlatform.OSXEditor)
+            return Platform.Mac;
+
+        if (platform == RuntimePlatform.Android)
+            return XRSettings.enabled ? Platform.MetaQuest : Platform.Android;
+
+        return Platform.Error;
+    }
+}
+
+public enum Platform { WindowsPC, WindowsVR, Mac, MetaQuest, Android, iOS, Error }
